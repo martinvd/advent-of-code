2984ecee0d2adb81ba0107d8844493006cde7b02
Martin van Driel <martinvd@gmail.com>
2023-12-07 01:29:47 +0100

    Simplify day 1 part 1

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -2,34 +2,18 @@ fn main() {
     let input = include_str!("./input.txt");
     let output = part1(input);
 
-    println!("Output: {}", output);
+    println!("Part 1: {}", output);
 }
 
 fn part1(input: &str) -> u32 {
-    let mut total = 0;
+    input
+        .lines()
+        .map(|line| {
+            let digits: Vec<u32> = line.chars().filter_map(|char| char.to_digit(10)).collect();
 
-    for line in input.lines() {
-        let mut first_num = String::new();
-
-        for char in line.chars() {
-            if char.is_numeric() {
-                first_num = char.to_string();
-                break;
-            }
-        }
-
-        let mut last_num = String::new();
-
-        for char in line.chars().rev() {
-            if char.is_numeric() {
-                last_num = char.to_string();
-                break;
-            }
-        }
-
-        let combined = first_num.to_owned() + &last_num.to_owned();
-        total += combined.parse::<u32>().unwrap();
-    }
-
-    total
+            (digits.first().unwrap().to_string() + &digits.last().unwrap().to_string())
+                .parse::<u32>()
+                .unwrap()
+        })
+        .sum()
 }
