0f7ac32f3325b964eac425bf4f3c892aa48b02d2
Martin van Driel <martinvd@gmail.com>
2024-12-25 22:44:50 +0100

    Add 2024 day 01 - part 2

--- 2024/day-01/src/main.rs
+++ 2024/day-01/src/main.rs
@@ -16,12 +16,20 @@ fn main() {
 
     left_numbers.sort();
     right_numbers.sort();
+
     let mut sum_of_diffs = 0;
+    let mut similarity_score: u32 = 0;
 
     for i in 0..left_numbers.len() {
-        let diff_abs = (left_numbers[i] as i32 - right_numbers[i] as i32).abs();
+        let left_num = left_numbers[i];
+        let diff_abs = (left_num as i32 - right_numbers[i] as i32).abs();
         sum_of_diffs += diff_abs;
+
+        let left_in_right_count =
+            right_numbers.iter().filter(|&num| *num == left_num).count() as u32;
+        similarity_score += left_num * left_in_right_count;
     }
 
     println!("Part 1: {}", sum_of_diffs);
+    println!("Part 2: {}", similarity_score);
 }
